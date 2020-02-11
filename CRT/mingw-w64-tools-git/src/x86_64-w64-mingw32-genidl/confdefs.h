3e74ae1bbc1b4a0124159f6e9fb1246fb3b7000d
beermix <27rurk@gmail.com>
2020-02-11 14:43:04 +0300

    autocommit on change

---
+++ CRT/mingw-w64-tools-git/src/x86_64-w64-mingw32-genidl/confdefs.h
@@ -0,0 +1,23 @@
+/* confdefs.h */
+#define PACKAGE_NAME "mingw-w64-genidl"
+#define PACKAGE_TARNAME "mingw-w64-genidl"
+#define PACKAGE_VERSION "1.0"
+#define PACKAGE_STRING "mingw-w64-genidl 1.0"
+#define PACKAGE_BUGREPORT "mingw-w64-public@lists.sourceforge.net"
+#define PACKAGE_URL ""
+#define PACKAGE "mingw-w64-genidl"
+#define VERSION "1.0"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_MALLOC_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
